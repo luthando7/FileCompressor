e7b79cc0a0d2022a0b41387446febb7f877fa4aa
Luthando Zaze <luthandozaze@gmail.com>
2024-06-19 00:07:42 +0200

    Readfile added, byte representation of file also added

--- src/main/java/Compressor.java
+++ src/main/java/Compressor.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class Compressor {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String fileName = System.getProperty("filePath");
+        Compressor compressor  = new Compressor();
+        byte[] fileContent = compressor.readFile(fileName);
+        System.out.println(Arrays.toString(fileContent));
+    }
+
+    private byte[] readFile(String fileName) {
+        try {
+            return Files.readAllBytes(Paths.get(fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
     }
 }
